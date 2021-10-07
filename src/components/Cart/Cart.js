f459fb7150f7471e5039e393d4f692fa38ebfc4d
unknown <anwarhosen575@gmail.com>
2021-10-07 22:18:09 +0600

    summary-spa-tax-total-grandtotal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,15 +4,24 @@ import './Cart.css';
 const Cart = (props) => {
     // console.log(props.cart);
     const { cart } = props;
-    let total = 0;
-    for (const product of cart) {
-        total = total + product.price;
-    }
+    const totalReducer = (previous, product) => previous + product.price;
+    const total = cart.reduce(totalReducer, 0);
+    // let total = 0;
+    // for (const product of cart) {
+    //     total = total + product.price;
+    // }
+    const shipping = 15;
+    const tax = (total + shipping) * 10;
+    const grandTotal = total + shipping + tax;
 
     return (
         <div>
             <h3>Order Summary</h3>
             <h5>Items Orders {props.cart.length}</h5>
+            <p>total : {total.toFixed(2)}</p>
+            <p>Shipping: {shipping}</p>
+            <p>tax : {tax.toFixed(2)}</p>
+            <p>GrandTotal {grandTotal.toFixed(2)}</p>
         </div>
     );
 };
